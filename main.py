6c80fbba8fe97217c10df518c9a1a457e63ae23b
Nikita Kouevda <nkouevda@gmail.com>
2012-11-09 20:54:29 -0800

    Add restful api.

--- main.py
+++ main.py
@@ -4,22 +4,89 @@
 # 2012/10/02
 
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # Flask
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/test"
+db = SQLAlchemy(app)
+
+from models import *
 
 @app.route("/")
 def root():
-    return "Hello, world!"
+    # TODO
 
-@app.route("/<sid>")
-def session(sid):
-    return "Session: %s" % sid
+    return "ROOT: Hello, world!"
+
+@app.route("/create_session", methods=["POST"])
+def create_session():
+    try:
+        session = Session()
+        db.session.add(session)
+
+        host = Person(session, request.form["name"], request.form["lat"], request.form["lon"])
+        db.session.add(host)
+
+        db.session.commit()
+
+        return jsonify(session.session_hash, host.id, error=0)
+    except:
+        return jsonify(error=1)
+
+@app.route("/create_person", methods=["POST"])
+def create_person():
+    try:
+        session = Session.query.filter_by(session_hash=request.form["session_hash"]).first()
+
+        if not session:
+            raise ValueError
+
+        person = Person(session, request.form["name"], request.form["lat"], request.form["lon"])
+        db.session.add(person)
+
+        db.session.commit()
+
+        return jsonify(person.id, error=0)
+    except:
+        return jsonify(error=1)
+
+@app.route("/<session_hash>")
+def session(session_hash):
+    return "SESSION: hash: %s" % session_hash
+
+@app.route("/<session_hash>/update", methods=["POST"])
+def session(session_hash):
+    try:
+        session = db.session.query.filter_by(session_hash=session_hash).first()
+
+        if not session:
+            raise ValueError
+
+        person = Person.query.filter_by(id=request.form["id"]).first()
+        person.lat = float(request.form["lat"])
+        person.lon = float(request.form["lon"])
+
+        db.session.commit()
+
+        return jsonify(error=0)
+    except:
+        return jsonify(error=1)
+
+@app.route("/<session_hash>/data", methods=["POST"])
+def session(session_hash):
+    try:
+        session = db.session.query.filter_by(session_hash=session_hash).first()
+        persons = [person.json() for person in session.persons]
+
+        return jsonify(persons=persons, centroid=session.centroid, lat=session.lat, lon=session.lon, error=0)
+    except:
+        return jsonify(error=1)
 
 @app.route("/<path:path>")
 def catch_all(path):
-    return "Path: %s" % path
+    return "CATCH: path: %s" % path
 
 if __name__ == "__main__":
     app.run()
--- models.py
+++ models.py
@@ -1,31 +1,40 @@
 # Nikita Kouevda, Eddie Lee, Anthony Sutardja
 # 2012/11/09
 
-class Session(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    persons = db.relationship("Person", backref="session", lazy="dynamic")
-    session_hash = db.Column(db.String())
-    lat = db.Column(db.Double)
-    lon = db.Column(db.Double)
+import random
+from string import ascii_letters, digits
+from main import db
 
-    def __init__(self, persons):
-        self.persons = persons
-        # TODO hash
-
-    def __repr__(self):
-        return "<Session %r>" % self.session_hash
+hash_chars = ascii_letters + digits
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     session_id = db.Column(db.Integer, db.ForeignKey("session.id"))
-    person_hash = db.Column(db.String())
+    session = db.relationship("Session", backref=db.backref("persons", lazy="dynamic"))
     name = db.Column(db.String())
-    lat = db.Column(db.Double)
-    lon = db.Column(db.Double)
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
 
-    def __init__(self, name):
+    def __init__(self, session, name, lat, lon):
+        self.session = session
         self.name = name
-        # TODO hash
+        self.lat = lat
+        self.lon = lon
 
     def __repr__(self):
         return "<Person %r>" % self.person_hash
+
+    def json(self):
+        return {"id": self.id, "name": self.name, "lat": self.lat, "lon": self.lon}
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    session_hash = db.Column(db.String())
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+
+    def __init__(self):
+        self.session_hash = ''.join(random.choice(hash_chars) for x in range(length))
+
+    def __repr__(self):
+        return "<Session %r>" % self.session_hash
