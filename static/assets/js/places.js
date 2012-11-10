4e1fb36c312a6cba377ddfa91917eac4de0b7e7b
Eddie Lee <eddieberklee@gmail.com>
2012-11-10 10:53:48 -0800

    zoom and center buttons

--- static/assets/js/places.js
+++ static/assets/js/places.js
@@ -2,6 +2,6 @@ function nearby_places(data) {
 	places = data.places;
 	for (var p in places) {
 		place = places[p]
-		$("#nearby_places_list").append("<li><div class='place' data-address='"+place.address+"'>"+place.name+"</div><div class='place' style='color:#bbb;font-size:0.7em;position:absolute;right:5px;'>"+place.address+"</div><div class='image'><img src=\""+place.image_url+"\"/></div></li>");
+		$("#nearby_places_list").append("<li><div class='place' data-address='"+place.address+"'>"+place.name+"</div><div class='place' style='color:#bbb;font-size:0.7em;position:absolute;right:5px;top:125px;'>"+place.address+"</div><div class='image'><img src=\""+place.image_url+"\"/></div></li>");
 	}
 }
